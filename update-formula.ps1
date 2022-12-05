468d11887682ebcf7f00096897bbbd82d254e303
Alexandre Zollinger Chohfi <alzollin@microsoft.com>
2022-12-05 14:41:52 -0800

    Updated script to work on older PS.

--- update-formula.ps1
+++ update-formula.ps1
@@ -15,12 +15,12 @@ $x64Content = Get-Content *x64*.sha256.txt
 $arm64Content = Get-Content *arm64*.sha256.txt
 $x64sha = $x64Content.Split("  ")[0]
 $arm64sha = $arm64Content.Split("  ")[0]
-if(!$x64sha || !$arm64sha) {
+if(!$x64sha -and !$arm64sha) {
     Write-Error 'Failed to get sha256'
     return;
 }
 
 Write-Output "Updating x64 sha to $x64sha, arm64 sha to $arm64sha, and version to $version"
 
-((Get-Content $PSScriptRoot\Formula\msstore-cli.rb -Raw) -replace 'MSSTORECLI_ARM64_URLSHA256',$arm64sha -replace 'MSSTORECLI_X64_URLSHA256',$x64sha -replace 'MSSTORECLI_VERSION', $version)
-    | Set-Content $PSScriptRoot\Formula\msstore-cli.rb
+$newForumla = ((Get-Content $PSScriptRoot\Formula\msstore-cli.rb -Raw) -replace 'MSSTORECLI_ARM64_URLSHA256',$arm64sha -replace 'MSSTORECLI_X64_URLSHA256',$x64sha -replace 'MSSTORECLI_VERSION', $version)
+$newForumla | Set-Content $PSScriptRoot\Formula\msstore-cli.rb
