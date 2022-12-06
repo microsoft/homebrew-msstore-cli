7a95106a7c76260479d59ef41d3eb1b4371d0e58
Alexandre Zollinger Chohfi <alzollin@microsoft.com>
2022-12-05 17:43:06 -0800

    Small fix.

--- Formula/msstore-cli.rb
+++ Formula/msstore-cli.rb
@@ -1,10 +1,9 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 
-class MSStoreCLI < Formula
+class MsstoreCli < Formula
     desc "Microsoft Store Command Line Tool"
     homepage "https://github.com/microsoft/msstore-cli"
-    url 
     url Hardware::CPU.arch == :arm64 ? "https://github.com/microsoft/msstore-cli/releases/download/v0.1.9/MSStoreCLI-osx.12-arm64.tar.gz" :
                                        "https://github.com/microsoft/msstore-cli/releases/download/v0.1.9/MSStoreCLI-osx.12-x64.tar.gz"
     sha256 Hardware::CPU.arch == :arm64 ? "b4c082952bac37481d9ea8ad3a102719aad910600975b0d5e6d2d42326c0e6db" :
