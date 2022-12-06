61a11ef5ea0c7eec69ca892125fbdfc892e68193
Alexandre Zollinger Chohfi <alzollin@microsoft.com>
2022-12-05 18:37:30 -0800

    Added linux support.

--- Formula/msstore-cli.rb
+++ Formula/msstore-cli.rb
@@ -4,12 +4,25 @@
 class MsstoreCli < Formula
     desc "Microsoft Store Command Line Tool"
     homepage "https://github.com/microsoft/msstore-cli"
-    url Hardware::CPU.arch == :arm64 ? "https://github.com/microsoft/msstore-cli/releases/download/v0.1.9/MSStoreCLI-osx.12-arm64.tar.gz" :
-                                       "https://github.com/microsoft/msstore-cli/releases/download/v0.1.9/MSStoreCLI-osx.12-x64.tar.gz"
-    sha256 Hardware::CPU.arch == :arm64 ? "b4c082952bac37481d9ea8ad3a102719aad910600975b0d5e6d2d42326c0e6db" :
-                                        "df4e1c6c5c797c889eb1ce175fba574a54e4157491277ad662033ac5dd529983"
     license "MIT"
     version "0.1.9"
+    checksums = {
+      "osx.12-arm64" => "d22d8fb336cc6d99a043fb3983238d56e486a7e6a95256cd3ffd30e9216bb8f0",
+      "osx.12-x64" => "46449452e1d35f38815468d68d622a45b35239b7eba85a5560fa3398ccc7b69c",
+      "linux-arm64"  => "b4c082952bac37481d9ea8ad3a102719aad910600975b0d5e6d2d42326c0e6db",
+      "linux-x64"  => "df4e1c6c5c797c889eb1ce175fba574a54e4157491277ad662033ac5dd529983"
+    }
+
+    os = OS.mac? ? "osx.12" : "linux"
+    arch = case Hardware::CPU.arch
+    when :x86_64 then "x64"
+    when :arm64 then "arm64"
+    else
+      raise "Unsupported arch #{Hardware::CPU.arch}"
+    end
+
+    url "https://github.com/microsoft/msstore-cli/releases/download/v#{version}/MSStoreCLI-#{os}-#{arch}.tar.gz"
+    sha256 checksums["#{os}-#{arch}"]
 
     def install
       bin.install "msstore"
--- update-formula.ps1
+++ update-formula.ps1
@@ -11,16 +11,20 @@ if ($version.StartsWith('v')) {
 
 Copy-Item $PSScriptRoot\msstore-cli.rb.template -Destination $PSScriptRoot\Formula\msstore-cli.rb -Force
 
-$x64Content = Get-Content *x64*.sha256.txt
-$arm64Content = Get-Content *arm64*.sha256.txt
-$x64sha = $x64Content.Split("  ")[0]
-$arm64sha = $arm64Content.Split("  ")[0]
-if(!$x64sha -and !$arm64sha) {
+$x64osxContent = Get-Content MSStoreCLI-osx.12-x64.tar.gz.sha256.txt
+$arm64osxContent = Get-Content MSStoreCLI-osx.12-arm64.tar.gz.sha256.txt
+$x64linuxContent = Get-Content MSStoreCLI-linux-x64.tar.gz.sha256.txt
+$arm64linuxContent = Get-Content MSStoreCLI-linux-arm64.tar.gz.sha256.txt
+$x64osxSha = $x64osxContent.Split("  ")[0]
+$arm64osxSha = $arm64osxContent.Split("  ")[0]
+$x64linuxSha = $x64linuxContent.Split("  ")[0]
+$arm64linuxSha = $arm64linuxContent.Split("  ")[0]
+if (!$x64osxSha -or !$arm64osxSha -or !$x64linuxSha -or !$arm64linuxSha) {
     Write-Error 'Failed to get sha256'
     return;
 }
 
-Write-Output "Updating x64 sha to $x64sha, arm64 sha to $arm64sha, and version to $version"
+Write-Output "Updating version to $version, x64osx: $x64osxSha, arm64osx: $arm64osxSha, x64linux: $x64linuxSha, arm64linux: $arm64linuxSha"
 
-$newForumla = ((Get-Content $PSScriptRoot\Formula\msstore-cli.rb -Raw) -replace 'MSSTORECLI_ARM64_URLSHA256',$arm64sha -replace 'MSSTORECLI_X64_URLSHA256',$x64sha -replace 'MSSTORECLI_VERSION', $version)
+$newForumla = ((Get-Content $PSScriptRoot\Formula\msstore-cli.rb -Raw) -replace 'MSSTORECLI_OSX_X64_URLSHA256',$x64osxSha -replace 'MSSTORECLI_OSX_ARM64_URLSHA256',$arm64osxSha -replace 'MSSTORECLI_LINUX_X64_URLSHA256',$x64linuxSha -replace 'MSSTORECLI_LINUX_ARM64_URLSHA256',$arm64linuxSha -replace 'MSSTORECLI_VERSION', $version)
 $newForumla | Set-Content $PSScriptRoot\Formula\msstore-cli.rb
