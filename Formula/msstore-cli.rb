266911aa9266d375c588316b477c88aa052acf28
MSStoreCLI Automated Account <msstore-cli-automated@microsoft.com>
2022-12-06 23:40:19 +0000

    Update formula with new release (v0.1.11).

--- Formula/msstore-cli.rb
+++ Formula/msstore-cli.rb
@@ -1,18 +1,16 @@
 # Documentation: https://docs.brew.sh/Formula-Cookbook
 #                https://rubydoc.brew.sh/Formula
 
-require_relative "../custom_download_strategy.rb"
-
 class MsstoreCli < Formula
     desc "Microsoft Store Command Line Tool"
     homepage "https://github.com/microsoft/msstore-cli"
     license "MIT"
-    version "0.1.9"
+    version "0.1.11"
     checksums = {
-      "osx.12-arm64" => "d22d8fb336cc6d99a043fb3983238d56e486a7e6a95256cd3ffd30e9216bb8f0",
-      "osx.12-x64" => "46449452e1d35f38815468d68d622a45b35239b7eba85a5560fa3398ccc7b69c",
-      "linux-arm64"  => "b4c082952bac37481d9ea8ad3a102719aad910600975b0d5e6d2d42326c0e6db",
-      "linux-x64"  => "df4e1c6c5c797c889eb1ce175fba574a54e4157491277ad662033ac5dd529983"
+      "osx.12-arm64" => "fb760432d12a49914dd6cefe49f8803a999689e4c79ea9072be106c6b65c735d",
+      "osx.12-x64" => "37807101721cc61bed2979fc6f286276ea4933ce7395d6e1850e4505685f7824",
+      "linux-arm64"  => "32d97fd3376c13d300e58569ba80436974a320f36fcd556c52b2169b798008b9",
+      "linux-x64"  => "a4127824fba04ce1bc38ce26e0f1dcc9083ea786ab7d7d22d381a087bf859966"
     }
 
     os = OS.mac? ? "osx.12" : "linux"
