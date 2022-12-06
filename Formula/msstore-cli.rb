db4ac491a38ab26bd9b1761c9ba0d42eab5e24d3
MSStoreCLI Automated Account <msstore-cli-automated@microsoft.com>
2022-12-06 00:18:08 +0000

    Update formula with new release (v0.1.9).

--- Formula/msstore-cli.rb
+++ Formula/msstore-cli.rb
@@ -5,12 +5,12 @@ class MSStoreCLI < Formula
     desc "Microsoft Store Command Line Tool"
     homepage "https://github.com/microsoft/msstore-cli"
     url 
-    url Hardware::CPU.arch == :arm64 ? "https://github.com/microsoft/msstore-cli/releases/download/v0.1.8/MSStoreCLI-osx.12-arm64.tar.gz" :
-                                       "https://github.com/microsoft/msstore-cli/releases/download/v0.1.8/MSStoreCLI-osx.12-x64.tar.gz"
-    sha256 Hardware::CPU.arch == :arm64 ? "2667d7e73a1a44c2a1549fdd33376ed7ced8e3718ec8703171df8e3178b32f0e" :
-                                        "d2ff11f95975312dc25d0332d655589018af841947e8730c58fb7947afb49891"
+    url Hardware::CPU.arch == :arm64 ? "https://github.com/microsoft/msstore-cli/releases/download/v0.1.9/MSStoreCLI-osx.12-arm64.tar.gz" :
+                                       "https://github.com/microsoft/msstore-cli/releases/download/v0.1.9/MSStoreCLI-osx.12-x64.tar.gz"
+    sha256 Hardware::CPU.arch == :arm64 ? "b4c082952bac37481d9ea8ad3a102719aad910600975b0d5e6d2d42326c0e6db" :
+                                        "df4e1c6c5c797c889eb1ce175fba574a54e4157491277ad662033ac5dd529983"
     license "MIT"
-    version "0.1.8"
+    version "0.1.9"
 
     def install
       bin.install "msstore"
