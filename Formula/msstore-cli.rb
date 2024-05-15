798858a6f35b9cff917d920214bd0a0aca38f799
Alexandre Zollinger Chohfi <alzollin@microsoft.com>
2024-05-15 20:23:20 +0000

    Update formula with new release (v0.1.32).

--- Formula/msstore-cli.rb
+++ Formula/msstore-cli.rb
@@ -2,12 +2,12 @@ class MsstoreCli < Formula
     desc "Microsoft Store Command Line Tool"
     homepage "https://github.com/microsoft/msstore-cli"
     license "MIT"
-    version "0.1.31"
+    version "0.1.32"
     checksums = {
-      "osx-arm64" => "4bcb90cf3ab26ea3be911ae1a196a6819b47edd134f4638eef83cb204b3b8cbe",
-      "osx-x64" => "551623bc18094660b10987cf6d1d3d066e7af53266c7eb6672f10c79ddb83237",
-      "linux-arm64"  => "140847a3bb0ff5e8a111d414ca1ac9fdec102589ad79f6d210d24215b8f7ae03",
-      "linux-x64"  => "5714098ea9415291aa74afad5ebbbc7e492e00360fdf0496a00c3f5bd296c54a"
+      "osx-arm64" => "74f71443ee1473878b3e677a6be7c9709862c7ac404657301b8a2f2879e596e9",
+      "osx-x64" => "de5b6b30cba2d9819452c096a6181d7f1045e2cb1179aaf70b2878ba8e4ab407",
+      "linux-arm64"  => "c49e7f6fa050ccd5d9aeb142a5daa15fb39cbd5e426651cb6c9dadabd90feb17",
+      "linux-x64"  => "181d14ff40757022953190aa22fffc2878992c63ae7557e5f23023224ced8537"
     }
 
     os = OS.mac? ? "osx" : "linux"
